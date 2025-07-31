bd11648f2b8f8e63c4647d1dc9dc24af51bf0c52
Samuel Letellier-Duchesne <sam@carbonsignal.com>
2025-07-31 11:28:43 -0400

    Auto stash before checking out "origin/main"

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from archetypal.idfclass import IDF
 
-idf = IDF()
+idf = IDF(file_version="23.1")
 obj = idf.newidfobject("SITE:LOCATION")
 obj.Time_Zone = 1
 
 for a in idf.idfobjects["SITE:LOCATION"]:
-    print(a)
+    print(a.Elevation)
